927aa8628f40fd9c23ee21d5f3e823d2e3dc3bf1
Arash Kaffashi <arash_kaffashi@hotmail.com>
2022-10-24 14:11:32 -0400

    done: Iteration 5 Solved lab

--- src/viking.js
+++ src/viking.js
@@ -74,4 +74,9 @@ class War {
 
 		return res;
 	}
+	showStatus() {
+		if (!this.saxonArmy.length) return "Vikings have won the war of the century!";
+		if (!this.vikingArmy.length) return "Saxons have fought for their lives and survived another day...";
+		return "Vikings and Saxons are still in the thick of battle.";
+	}
 }
